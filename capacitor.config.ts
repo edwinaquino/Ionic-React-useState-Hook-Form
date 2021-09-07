99f49764aad1ad322a4ca05c8c3af66ebd4d485c
edwinaquino <github@edwinaquino.com>
2021-09-06 18:31:35 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'two-way-binding-with-usestate',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
